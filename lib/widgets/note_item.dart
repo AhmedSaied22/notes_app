d31b776c377a8b5db088ff03562b7bab6a186f2c
AhmedSaied22 <ahmedsaied2016201@gmail.com>
2024-02-27 17:55:38 +0200

    delete note

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -46,7 +46,9 @@ class NoteItem extends StatelessWidget {
                   ),
                 ),
                 trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    note.delete();
+                  },
                   icon: const Icon(
                     FontAwesomeIcons.trash,
                     color: Colors.black,
